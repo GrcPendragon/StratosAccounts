63ee594fc7ef8376011ced8f35a6949f8e22d1cb
FAUSTO ISRAEL GARCIA GARCIA <fausto.garcia@alumnos.udg.mx>
2020-09-08 22:02:04 -0500

    Se integraron componentes a la interfaz [Directory]. Elemento principal: Tabla. Elementos secundarios: 3 textField para los datos de prueba y un botón para agregar los datos a la tabla. Además se integran una clase en el package [model] Directory para el proceso de captura de datos para ser incrustados en la tabla.

--- src/controller/DirectoryController.java
+++ src/controller/DirectoryController.java
@@ -3,18 +3,24 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
+import model.Directory;
 
 /**
  * FXML Controller class
@@ -23,14 +29,62 @@ import javafx.scene.layout.AnchorPane;
  */
 public class DirectoryController implements Initializable {
 
-   
+    @FXML
+    private Button btnIngresar;
+    @FXML
+    private TextField txtPagina;
+    @FXML
+    private PasswordField txtPass;
+    @FXML
+    private TextField txtCorreo;
+    @FXML
+    private TableView<Directory> tblDirectory;
+    @FXML
+    private TableColumn colPagina;
+    @FXML
+    private TableColumn colCorreo;
+    @FXML
+    private TableColumn colPass;
+
+    private ObservableList<Directory> directorio;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
-    }    
+        directorio = FXCollections.observableArrayList();
 
+        this.colPagina.setCellValueFactory(new PropertyValueFactory("pagina"));
+        this.colCorreo.setCellValueFactory(new PropertyValueFactory("correo"));
+        this.colPass.setCellValueFactory(new PropertyValueFactory("pass"));
+    }
+
+    @FXML
+    private void click(ActionEvent event) {
+        String pagina = this.txtPagina.getText();
+        String correo = this.txtCorreo.getText();
+        String pass = this.txtPass.getText();
+
+        Directory registro = new Directory(pagina, correo, pass);
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        
+        try {
+            if (!this.directorio.contains(registro)) {
+                this.directorio.add(registro);
+                this.tblDirectory.setItems(directorio);
+            } else {
+                alert.setHeaderText(null);
+                alert.setTitle("Error!");
+                alert.setContentText("El registro ya existe.");
+                alert.show();
+            }
+        } catch (Exception e) {
+            alert.setHeaderText(null);
+            alert.setTitle("Error!");
+            alert.setContentText(e.toString());
+            alert.show();
+        }
+    }
 
 }
---
+++ src/model/Directory.java
@@ -0,0 +1,75 @@
+package model;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author Fausto
+ */
+public class Directory {
+    private String pagina;
+    private String correo;
+    private String pass;
+
+    public Directory(String pagina, String correo, String pass) {
+        this.pagina = pagina;
+        this.correo = correo;
+        this.pass = pass;
+    }
+
+    public String getPagina() {
+        return pagina;
+    }
+
+    public void setPagina(String pagina) {
+        this.pagina = pagina;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Directory other = (Directory) obj;
+        if (!Objects.equals(this.pagina, other.pagina)) {
+            return false;
+        }
+        if (!Objects.equals(this.correo, other.correo)) {
+            return false;
+        }
+        if (!Objects.equals(this.pass, other.pass)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+}
